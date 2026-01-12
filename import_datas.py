e45b9a264aecb8987c0219b2bf323f66b8e1293a
frst_304 <130504630+frst304@users.noreply.github.com>
2026-01-12 12:19:53 +0100

    edit files

--- import_datas.py
+++ import_datas.py
@@ -3,21 +3,12 @@ import requests
 from io import StringIO
 
 
-def telecharger_fichier_csv_depuis_url(url):
-    response = requests.get(url, verify=True)
-    response.raise_for_status()
-    return response
-
-
-def lire_csv_depuis_reponse_http(reponse_http, separateur=","):
-    dataframe = pd.read_csv(StringIO(reponse_http.text), sep=separateur, low_memory=False)
-    return dataframe
-
-
 def importer_donnees_covid19():
     url_donnees_covid = "https://static.data.gouv.fr/resources/synthese-des-indicateurs-de-suivi-de-lepidemie-covid-19/20230630-155906/table-indicateurs-open-data-france-2023-06-30-17h59.csv"
     
-    reponse = telecharger_fichier_csv_depuis_url(url_donnees_covid)
-    dataframe = lire_csv_depuis_reponse_http(reponse, separateur=",")
+    response = requests.get(url_donnees_covid, verify=True)
+    response.raise_for_status()
+    
+    dataframe = pd.read_csv(StringIO(response.text), sep=",", low_memory=False)
     
     return dataframe
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from import_datas import importer_donnees_covid19
-from traitement_donnees import preparer_donnees_pour_graphique_deces
-from graphique import afficher_graphique_dans_fenetre
-from gestion_erreurs import afficher_message_erreur
 from tkinter_config import creer_fenetre
 
 
 def main():
-    try:
-        donnees_brutes = importer_donnees_covid19()
-        donnees_preparees = preparer_donnees_pour_graphique_deces(donnees_brutes)
-        
-        fenetre = creer_fenetre()
-        afficher_graphique_dans_fenetre(fenetre, donnees_preparees)
-        
-        fenetre.mainloop()
-    except Exception as erreur:
-        afficher_message_erreur(erreur)
+    donnees = importer_donnees_covid19()
+    fenetre = creer_fenetre()
+    fenetre.mainloop()
 
 
 if __name__ == "__main__":
